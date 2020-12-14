3e241011dbaa739bffe5c7b0e3fc5916efde18b2
johannchopin <johannchopin@pm.me>
2020-12-14 13:07:45 +0100

    🚚 Add Navbar component

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { onMount } from 'svelte'
 	import RouteSection from './components/RouteSection.svelte'
+	import Navbar from './components/Navbar.svelte'
 	import Sidebar from './components/Sidebar.svelte'
 
   import { METHODS } from './const'
@@ -29,50 +30,12 @@
 			console.log(error);
 		})
 	})
-
-	const close = (): void => {
-		api.get('/close')
-	}
-
 </script>
 
+<Navbar />
 <div class="d-flex" class:toggled={isSidebarToggled} id="wrapper">
-
 	<Sidebar />
-
 	<div id="page-content-wrapper">
-
-		<nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
-			<button class="btn btn-primary" on:click={() => {isSidebarToggled = !isSidebarToggled}}>Toggle Menu</button>
-			<button class="btn btn-danger" on:click={close}>Close server</button>
-
-			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-				<span class="navbar-toggler-icon"></span>
-			</button>
-
-			<div class="collapse navbar-collapse" id="navbarSupportedContent">
-				<ul class="navbar-nav ml-auto mt-2 mt-lg-0">
-					<li class="nav-item active">
-						<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="#">Link</a>
-					</li>
-					<li class="nav-item dropdown">
-						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-							Dropdown
-						</a>
-						<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-							<a class="dropdown-item" href="#">Action</a>
-							<a class="dropdown-item" href="#">Another action</a>
-							<div class="dropdown-divider"></div>
-							<a class="dropdown-item" href="#">Something else here</a>
-						</div>
-					</li>
-				</ul>
-			</div>
-		</nav>
-
 		<div class="container-fluid" id="content">
 			{#if routes}
 				{#each Object.keys(routes) as route}
@@ -92,7 +55,8 @@
 
 	#wrapper {
 		overflow-x: hidden;
-		height: 100%;
+		max-height: 100%;
+    overflow: hidden;
 	}
 
 	#content {
---
+++ src/components/Navbar.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import Icon from './Icon/Icon.svelte'
+</script>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <h1>
+    <a class="navbar-brand" href="/">Restapify</a>
+  </h1>
+  <ul class="navbar-nav ml-auto nav-flex-icons">
+    <li class="nav-item">
+      <a class="nav-link" href="https://github.com/johannchopin/restapify" target="_blank">
+        <Icon name='github'/>
+      </a>
+    </li>
+    <li class="nav-item">
+      <button id="toggleTheme" class="nav-link">
+        <Icon name='circle-half'/>
+      </button>
+    </li>
+  </ul>
+</nav>
+
+<style lang="scss">
+  .navbar-nav {
+    li {
+      #toggleTheme {
+        border: none;
+      }
+    }
+  }
+</style>
