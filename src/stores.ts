1ce3f2bc3bb90f1da7df382b080c2cbb226b885b
johannchopin <johannchopin@pm.me>
2020-12-21 00:35:34 +0100

    ✨ Create and use Alert

--- src/stores.ts
+++ src/stores.ts
@@ -1,6 +1,12 @@
 import { writable } from 'svelte/store';
 import type { GetRoutesResponse, Theme, GetStatesResponse, StateResponse, HttpMethod } from './types';
 
+interface AlertStore {
+	show: boolean
+	type: 'success' | 'error' | 'warning'
+	message: string
+}
+
 const createThemeStore = () => {
   const { subscribe, update } = writable<Theme>({ mode: 'light' })
 
@@ -31,6 +37,26 @@ const createStatesStore = () => {
 	}
 }
 
+const createAlertStore = () => {
+	const { subscribe, update, set } = writable<AlertStore>({type: 'success', show: false, message: ''})
+
+	return {
+		subscribe,
+		show: (data: Omit<AlertStore, 'show'>): void => {
+			set({
+				show: true,
+				...data
+			})
+		},
+		hide: () => {
+			update((value) => ({...value, show: false}))
+		},
+		update,
+		set
+	}
+}
+
 export const theme = createThemeStore()
 export const routes = writable<GetRoutesResponse | null>(null)
 export const states = createStatesStore()
+export const alert = createAlertStore()
