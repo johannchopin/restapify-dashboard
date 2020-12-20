a53766bc34f37ee85b46ee535623ff2281fa018a
johannchopin <johannchopin@pm.me>
2020-12-20 16:24:40 +0100

    ✨ Add close Icon

--- src/components/Icon/Icon.svelte
+++ src/components/Icon/Icon.svelte
@@ -1,20 +1,21 @@
 <script lang="ts">
   import link from './icons/link.svg'
   import github from './icons/github.svg'
+  import close from './icons/close.svg'
   import circleHalf from './icons/circle-half.svg'
 
   let className = ''
 
-  type Name = 'link' | 'github' | 'circle-half'
+  type Name = 'link' | 'github' | 'circle-half' | 'close'
 
   export let name: Name = 'github'
   export { className as class }
 
-
   const icons: {[name in Name]: any} = {
     'circle-half': circleHalf,
-    'link': link,
-    'github': github
+    link: link,
+    github: github,
+    close: close
   }
 </script>
 
