71a5f5b48e1e50ce162bf865403fc7a962e420c8
johannchopin <johannchopin@pm.me>
2020-12-29 00:14:44 +0100

    ⬆️ Use latest restapify version

--- src/axiosStore.ts
+++ src/axiosStore.ts
@@ -5,8 +5,13 @@ const port = (__NODE_ENV__ === 'development'
   ? 6767 
   : new URL(window.location.href).port)
 
+// @ts-ignore
+const baseUrl = (__NODE_ENV__ === 'development' 
+? '/dev/restapify/api' 
+: '/restapify/api')
+
 const instance = axios.create({
-  baseURL: `http://localhost:${port}/restapify/api`
+  baseURL: `http://localhost:${port}${baseUrl}`
 })
 
 export default instance
