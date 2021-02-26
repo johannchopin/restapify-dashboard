8ccf5493265c817436506b559a96286d8dc9aeee
johannchopin <johannchopin@pm.me>
2021-02-26 22:56:07 +0100

    ✨ Playground response

--- src/components/RouteRequestPlayground.svelte
+++ src/components/RouteRequestPlayground.svelte
@@ -1,18 +1,16 @@
 <script lang="ts">
+  import { theme as themeStore } from '../stores'
   import MethodBadge from './MethodBadge.svelte'
-  import { getRouteStructure } from '../utils'
+  import RouteRequestPlaygroundResponse from './RouteRequestPlaygroundResponse.svelte'
+  import type { Response } from './RouteRequestPlaygroundResponse.svelte'
+
+  import { getRouteStructure, replaceAll } from '../utils'
   import type { HttpMethod } from '../types'
 
 	import api from '../axiosStore'
 
   export let method: HttpMethod
   export let route: string
-
-  type Response {
-    status: number
-    header: Object
-    body: null | Object | any[]
-  }
 
   const routeStructure = getRouteStructure(route)
 
@@ -25,56 +23,77 @@
     let requestCall = `http://localhost:6768/api/${request.join('/')}`
     if (queries) requestCall += `?${queries}`
     api[method.toLowerCase()](requestCall)
-      .then((response) => {
-        console.log(response)
+      .then(({data, headers, status}) => {
+        response = {
+            status,
+            headers,
+            body: data
+          }
+      })
+      .catch((error) => {
+        if (error.response) {
+          const { status, headers, data } = error.response
+          response = {
+            status,
+            headers,
+            body: data
+          }
+          console.log(error.response);
+        }
       })
   }
 </script>
 
-<div class="playground row">
-  <div class="d-flex align-items-center">
-    <div class="request d-flex align-items-center border border-1 border-secondary p-2 rounded-2">
-      <MethodBadge {method} />
-      <div class="d-flex ms-3">
-        <nav aria-label="breadcrumb">
-          <ol class="breadcrumb mb-0">
-            {#each routeStructure as routeComponent, i}
-              <li class="breadcrumb-item d-flex align-items-center ps-1">
-                {#if routeComponent.type === 'variable'}
-                  <input 
+<div class="playground row mt-4">
+  <form on:submit|preventDefault={fetch}>
+    <div class="d-flex align-items-center">
+      <div class="request d-flex align-items-center border border-1 border-secondary p-2 rounded-2">
+        <MethodBadge {method} />
+        <div class="d-flex ms-3">
+          <nav aria-label="breadcrumb">
+            <ol class="breadcrumb mb-0">
+              {#each routeStructure as routeComponent, i}
+                <li class="breadcrumb-item d-flex align-items-center ps-1">
+                  {#if routeComponent.type === 'variable'}
+                    <input
+                      type="text"
+                      class={`form-control mb-0 py-0 px-1 bg-${$themeStore.mode}`}
+                      placeholder={routeComponent.value}
+                      required
+                      on:input={(event) => {
+                        const requestClone = [...request]
+                        requestClone[i] = event.target.value
+                        request = requestClone
+                      }}
+                    />
+                  {:else}
+                    {routeComponent.value}
+                  {/if}
+                </li>
+              {/each}
+              <li class="d-flex align-items-center ps-1">
+                  ?<input 
                     type="text" 
-                    class="form-control mb-0 py-0 px-1" 
-                    placeholder={routeComponent.value} 
-                    on:input={(event) => {
-                      const requestClone = [...request]
-                      requestClone[i] = event.target.value
-                      request = requestClone
-                    }}
-                  />
-                {:else}
-                  {routeComponent.value}
-                {/if}
+                    class={`form-control queries mb-0 py-0 px-1 bg-${$themeStore.mode}`} 
+                    placeholder="queries..."
+                    bind:value={queries}
+                  >
               </li>
-            {/each}
-            <li class="d-flex align-items-center ps-1">
-                ?<input 
-                  type="text" 
-                  class="form-control queries mb-0 py-0 px-1" 
-                  placeholder="queries..."
-                  bind:value={queries}
-                >
-            </li>
-          </ol>
-        </nav>
+            </ol>
+          </nav>
+        </div>
       </div>
+      <button 
+        type="submit" 
+        class="btn btn-outline-success mb-0 ms-2"
+        >Send
+      </button>
     </div>
-    <button 
-      type="button" 
-      class="btn btn-outline-primary mb-0 ms-2"
-      on:click={fetch}
-      >Send
-    </button>
-  </div>
+  </form>
+
+  {#if response}
+     <RouteRequestPlaygroundResponse id={`play-${method}${replaceAll(replaceAll(replaceAll(route, '/', '-'), '[', '-'), ']', '-')}`} {response} />
+  {/if}
 </div>
 
 <style>
