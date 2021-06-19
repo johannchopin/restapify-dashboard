8b9ea045541269a70d2a71eb7e78d335a112944e
johannchopin <johannchopin@pm.me>
2021-06-19 13:32:45 +0200

    ✨ Add footer with author

---
+++ src/components/Footer.svelte
@@ -0,0 +1,13 @@
+<footer class="d-flex flex-column align-items-center mt-4">
+  <p class="mb-0 ms-2 text-nowrap text-truncate">Maintained with ❤️ &nbsp;by <a href="https://github.com/johannchopin" target="_blank" class="text-info">@johannchopin</a></p>
+</footer>
+
+<style>
+  footer {
+    width: 100%
+  }
+  p {
+    font-size: .8em;
+    max-width: 100%
+  }
+</style>
